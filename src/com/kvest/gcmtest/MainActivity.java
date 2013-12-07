2d3658d0de8cbcef619b741c652b82f2dca9fb3b
Roman Bielokon <roman_slavyansk@mail.ru>
2013-12-07 23:06:36 +0200

    added play services library

--- src/com/kvest/gcmtest/MainActivity.java
+++ src/com/kvest/gcmtest/MainActivity.java
@@ -2,8 +2,13 @@ package com.kvest.gcmtest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 public class MainActivity extends Activity {
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +16,30 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // Check device for Play Services APK.
+        if (checkPlayServices()) {
+        }
+    }
+
+    // You need to do the Play Services APK check here too.
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPlayServices();
+    }
+
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i("KVEST_TAG", "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 }
